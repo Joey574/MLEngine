80f88da316092842b0d403f87b71de680bdc67fe
Joey574 <joeysoroka@outlook.com>
2025-11-03 16:17:52 -0800

    working maybe?

--- cmake/OpenBLASFetch.cmake
+++ cmake/OpenBLASFetch.cmake
@@ -1,23 +1,29 @@
 # OpenBLASFetch.cmake
 include(FetchContent)
-find_package(OpenBLAS QUIET)
 
-set(NO_SHARED TRUE)
-set(USE_OPENMP TRUE)
-set(ONLY_CBLAS TRUE)
-
-set(BUILD_DOUBLE OFF)
-set(BUILD_COMPLEX OFF)
+set(BLA_VENDOR OpenBLAS)
+find_package(BLAS QUIET)
 
 if(NOT OpenBLAS_FOUND AND NOT TARGET OpenBLAS::OpenBLAS)
     message(STATUS "Fetching OpenBLAS")
-    #set(FETCHCONTENT_QUIET TRUE)
+
+    set(CMAKE_VERBOSE_MAKEFILE OFF CACHE BOOL "" FORCE)
+    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -w -Wno-unused-variable -Wno-unused-function")
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -w -Wno-unused-variable -Wno-unused-function")
+
+    set(NO_SHARED TRUE)
+    set(USE_OPENMP TRUE)
+    set(ONLY_CBLAS TRUE)
+    set(BUILD_COMPLEX OFF)
+    set(BUILD_COMPLEX16 OFF)
 
     FetchContent_Declare(
-        OpenBLAS
+        openblas
         GIT_REPOSITORY https://github.com/xianyi/OpenBLAS.git
         GIT_TAG develop
         GIT_SHALLOW TRUE
+        QUIET
     )
-    FetchContent_MakeAvailable(OpenBLAS)
+
+    FetchContent_MakeAvailable(openblas)
 endif()
