1779e92d8d827428afa0ba2e686a984787bacc4b
Joey574 <joeysoroka@outlook.com>
2025-11-16 15:18:08 -0800

    trying to programatically set OMP_NUM_THREADS, not working

--- src/main.cpp
+++ src/main.cpp
@@ -50,10 +50,26 @@ void segv(int signum) {
     exit(1);
 }
 
+// TODO : Actually get this to work
+__attribute__((constructor(101)))
+static void configure() {
+    unsetenv("OMP_NUM_THREADS");
+    unsetenv("OPENBLAS_NUM_THREADS");
+
+    int logical = std::thread::hardware_concurrency();
+    int threads = logical / 2;
+
+    omp_set_num_threads(threads);
+    setenv("OMP_NUM_THREADS", std::to_string(threads).c_str(), 1);
+}
+
 int main(int argc, char* argv[]) {
     KEEPRUNNING = true;
     signal(SIGINT, handleInterupt);
     signal(SIGSEGV, segv);
+
+    int ideal = std::thread::hardware_concurrency() / 2;
+    omp_set_num_threads(ideal);
 
     #ifdef DEBUG
         std::cout << "[i] Running in DEBUG mode\n";
